f8913e03c2cad4d497804441207394c646455011
ruizqa <89569188+ruizqa@users.noreply.github.com>
2021-09-11 00:13:11 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+// I thought we had to make random pizzas at first so ignore first block of code. See comment below
+
+let crustTypes;
+let sauceTypes;
+let Cheeses;
+let Toppings;
+let n=1;
+
+crustTypes = ["Stuffed", "Cracker", "Flat Bread", "Thin", "Cheese", "Thick"];
+sauceTypes = ["Spicy Red", "Peppery Red", "Sweet Pizza", "Pesto Sauce", "Creamy Alfredo Sauce", "BBQ"];
+Cheeses = ["Mozzarella", "Cheddar/Matured Cheddar", "Aged Havarti","Gorgonzola","Provolone","Goat cheese"];
+Toppings = ["Garlic", "Chili peppers", "Oregano", "Tuna", "Pepperoni","Ham","Pineapple","Mushrooms" ];
+
+ingredients_list = [crustTypes,sauceTypes, Cheeses, Toppings];
+
+getIngredients = function(ingredients_list){
+    ingredients = [];
+
+    for (i =0; i< ingredients_list.length; i++){
+    
+        ingredients.push(ingredients_list[i][Math.floor(Math.random()*ingredients_list[i].length)])
+    
+    }
+
+    return ingredients;
+
+}
+
+
+
+function pizzaOvenRandom(ingredients_list) {
+    let ingredients;
+    ingredients = getIngredients(ingredients_list);
+    var pizza = {};
+    pizza.crust = ingredients[0];
+    pizza.sauce = ingredients[1];
+    pizza.cheeses = ingredients[2];
+    pizza.toppings = ingredients[3];
+    return pizza;
+}
+
+//Read from this point forward...
+
+function pizzaOven(crustType, sauceType, cheeses, toppings){
+
+    var pizza = {};
+    pizza.crust = crustType;
+    pizza.sauce = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+
+
+}
+
+
+console.log("Pizza "+ n + ": \n");
+pizza1 = pizzaOven("deep dish", "traditional", ["mozzarella"],["pepperoni", "sausage"]);
+console.log(pizza1);
+n++
+console.log("Pizza "+ n + ": \n");
+pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(pizza2);
+n++
+
+console.log("Let's create two random pizzas!\n")
+console.log("Pizza "+ n + ": \n");
+pizza3 = pizzaOvenRandom(ingredients_list);
+console.log(pizza3);
+n++
+
+console.log("Pizza "+ n + ": \n");
+pizza4 = pizzaOvenRandom(ingredients_list);
+console.log(pizza4);
